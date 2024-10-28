eba28ff338d0686472a5fe5d34d003af63b4861d
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-10-28 14:49:18 +0200

    Hello World

---
+++ Assets/Scripts/HexagonScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class HexagonScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+}
+/* Д.З. Наповнити композицію сцени проєкту
+ * (розмістити різні спрайти, підібрати фон, тощо)
+ */
