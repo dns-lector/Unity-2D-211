c16c8362081cf2426f27b9c4dba55cd9ab29cc05
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-11-04 15:05:14 +0200

    Physics

--- Assets/Scripts/HexagonScript.cs
+++ Assets/Scripts/HexagonScript.cs
@@ -2,11 +2,20 @@
 
 public class HexagonScript : MonoBehaviour
 {
+    private Rigidbody2D rb2d;   // посилання на об'єкт компонента
+
     void Start()
     {
-        Debug.Log("Hello World!");
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * 300);
+        }
+    }
 }
 /* Д.З. Наповнити композицію сцени проєкту
  * (розмістити різні спрайти, підібрати фон, тощо)
